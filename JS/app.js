4a2bc970d185dda0b607af842e0cc94f2d8c4f58
luisSandino <sandino.ruiz@gmail.com>
2016-01-29 09:48:52 -0600

    primer commit

---
+++ JS/app.js
@@ -0,0 +1,28 @@
+angular.module('myToDoList', ['ngStorage'] )
+.controller('todoController', function($scope, $localStorage) {
+
+
+
+	
+	$scope.listas= [];
+
+	$scope.agregarTarea= function() {
+		$scope.listas.push($scope.nuevaTarea);
+		$scope.nuevaTarea={};
+		
+		
+	};
+
+	
+
+	$scope.borrarTarea= function(index) {
+		$scope.listas.splice(index, 1);
+		
+	};
+
+	
+
+   
+});
+
+
